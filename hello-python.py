6ec347cd27738b56109e2d876e18294c7ca2d6d1
平民·寻梦（Pingmin Fenlly Liu） <Pingmin@Fenlly.org>
2021-09-17 11:51:03 +0800

    Add the test hello-python.py

---
+++ hello-python.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+# 第一个注释
+# 第二个注释
+
+'''
+第三注释
+第四注释
+'''
+
+"""
+第五注释
+第六注释
+"""
+print ("Hello, Python!")
+
+
+if True:
+    print ("Answer")
+    print ("True")
+else:
+    print ("Answer")
+    #print ("False")
+  #print ("False")
+
+print ("False")
